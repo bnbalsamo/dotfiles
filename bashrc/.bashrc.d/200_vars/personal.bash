fb2c24f22f0d4801892e6188a35f128df2e5892b
Brian Balsamo <Brian@BrianBalsamo.com>
2021-03-02 17:51:22 -0600

    Adds a .pythonrc
    This file uses the python package rich (if it is installed) to make some
    REPL output prettier/more readable by default.

--- bashrc/.bashrc.d/200_vars/personal.bash
+++ bashrc/.bashrc.d/200_vars/personal.bash
@@ -5,9 +5,14 @@ if [[ -d ${HOME}/.local/bin && ${PATH} != *${HOME}/.local/bin* ]] ; then
         export PATH=${HOME}/.local/bin:"${PATH}:"
 fi
 
+# Use a python startup file if one exists
+if [[ -e $HOME/.pythonrc ]]; then export PYTHONSTARTUP=$HOME/.pythonrc; fi
+
+
 # Some handy global vars for opening things
 export EDITOR=$( ( [[ `type -t vim` ]] && which vim ) || \
                  ( [[ `type -t vi` ]] && which vi ) || \
                  ( [[ `type -t nano` ]] && which nano) )
 export BROWSER=$( ( [[ `type -t firefox` ]] && which firefox ) || \
                   ( [[ `type -t google-chrome-stable` ]] && which google-chrome-stable) )
+
